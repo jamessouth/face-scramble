bfa2d5c704d381fca2031b38f2af35fe47f68e52
jamessouth <jamesdanielsouth@gmail.com>
2018-11-14 02:19:58 -0600

    begin options

--- FaceScramble/src/app/home/home.component.ts
+++ FaceScramble/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
         // Init your component properties here.
     }
 
-    newPhoto(): void {
+    enter(): void {
       alert('new photo');
     }
 }
---
+++ FaceScramble/src/app/opts/opts.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "Opts",
+  templateUrl: "./opts/opts.component.html",
+  styleUrls: ["./opts/opts.component.css"]
+})
+export class OptsComponent {}
